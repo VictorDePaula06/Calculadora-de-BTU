3e681c530c6da4d1ec7a5468c3aeecaa3e115502
João victor de paula <j.17jvictor@gmail.com>
2023-02-20 18:25:16 -0300

    adição de novas funções para melhorar o cálculo e experiência do usuário

--- script.js
+++ script.js
@@ -7,24 +7,68 @@
 // variaveis
 
 
-let metros = document.getElementById("metros");
-let pessoas = document.getElementById("pessoas");
-let eletro = document.getElementById("eletro")
-let res = document.getElementsByClassName("resultado")[0];
+const metros = document.getElementById("metros");
+const pessoas = document.getElementById("pessoas");
+const eletro = document.getElementById("eletro")
+const opS = document.getElementById("sim")
+const opN = document.getElementById("nao")
 
-//função
+function exibirMensagemErro(mensagem){
+    Swal.fire({
+        icon: 'error',
+        title: 'Opsss...',
+        text: mensagem
+      });
+}
+
+function exibirResultado(resultado){
+    Swal.fire({
+        icon: 'success',
+        title: 'Calculo efetuado...',
+        text: resultado
+      });
+}
 
 function calcular(){
+     // Calculo feito sem exposição ao sol
+    if(opN.checked){
+        const calcMetros = Number(metros.value * 600)
+        const calcPessoas = Number((pessoas.value - 1) * 600)
+        const calcEletro = Number(eletro.value * 600)
+        
 
-
-    let calcMetros = Number(metros.value * 600)
-    let calcPessoas = Number((pessoas.value - 1) * 600)
-    let calcEletro = Number(eletro.value * 600)
-    let calculoTotal = calcMetros + calcPessoas + calcEletro
-    if(calcMetros <= 0 || calcEletro <= 0 ||  calcEletro <= 0){
-        alert("[ERROR] Por favor insira um valor para afetuar o calculo!")
-    }else{
-        res.innerHTML = calculoTotal + " BTUs "
+        if(isNaN(calcMetros) || isNaN(calcPessoas) || isNaN(calcEletro)){
+            }else if(calcMetros <= 0 || calcEletro <= 0 ||  calcPessoas < 0){
+                exibirMensagemErro("[ERROR] Por favor insira um valor para afetuar o calculo!")
+            }else{
+                const calculoTotal = calcMetros + calcPessoas + calcEletro
+                exibirResultado(calculoTotal + " BTUs ")
+            }
     }
-    
+     else if(opS.checked){
+        // Calculo feito com exposição ao sol
+        const calcMetros = Number(metros.value * 600)
+        const calcPessoas = Number((pessoas.value - 1) * 600)
+        const calcEletro = Number(eletro.value * 600)
+        
+
+        if(isNaN(calcMetros) || isNaN(calcPessoas) || isNaN(calcEletro)){
+            }else if(calcMetros <= 0 || calcEletro <= 0 ||  calcPessoas < 0){
+                exibirMensagemErro(["[ERROR] Por favor insira um valor para afetuar o calculo!"])
+            }else{
+                const calculoTotal =  calcMetros + calcPessoas + calcEletro + 800 
+                exibirResultado(calculoTotal + " BTUs ")
+            }
+           
+    }
+
 }
+
+function limparCampos(){
+    inputs.forEach(input=>{
+        input.value = "";
+    })
+}
+
+const inputs = [metros,pessoas,eletro];
+limparCampos(inputs);
